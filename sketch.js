51fee7731d85d8015288a92fe743dd1b6c1ab0de
AdityaKantipudi <akantipudi00@gmail.com>
2020-03-30 20:56:55 +0530

    We learnt about the api call

--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var bg;
+var score =0;
 
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImage();
 }
 
 function setup(){
@@ -45,18 +47,23 @@ function setup(){
 }
 
 function draw(){
+    if (backgroundImg)
     background(backgroundImg);
+    fill("white");
+    text("Score:"+ score,width - 300,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -86,3 +93,65 @@ function keyPressed(){
        // slingshot.attach(bird.body);
     }
 }
+  async function getBackgroundImage () {
+   var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+   var responseJson = await response.json();
+   console.log(responseJson);
+    var datetime = responseJson.datetime; 
+    var hour = datetime.slice(11,13);
+     console.log(hour);
+     if (hour >= 06 && hour <= 19) {
+     bg = "sprites/bg.png";
+
+
+     }
+
+     else {
+         bg = "sprites/bg2.jpg"
+     }
+     backgroundImg = loadImage (bg);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
